2098181446be760ffe45b2a9fdd60561ddf44642
Dariusz Okoński <darek200180@gmail.com>
2025-05-20 15:04:51 +0200

    simplyfy bootcamps

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,14 +9,11 @@ const {
 
 const router = express.Router();
 
-router.get('/', getBootcamps);
-
-router.get('/:id', getBootcamp);
-
-router.post('/', createBootcamp);
-
-router.put('/:id', updateBootcamp);
-
-router.delete('/:id', deleteBootcamp);
+router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
