fc74c04fd13d08e87453f066c82af490c3dc4619
Dariusz Okoński <darek200180@gmail.com>
2025-05-21 14:10:51 +0200

    add colors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -23,12 +24,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`.yellow
+      .bold
   );
 });
 
 // handle unhandled rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   server.close(() => process.exit(1));
 });
