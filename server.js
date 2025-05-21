39fe55ef631310cb281271da62480ea792a6cc97
Dariusz Okoński <darek200180@gmail.com>
2025-05-21 14:01:40 +0200

    connect to mongoDB

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ dotenv.config({ path: './config/config.env' });
 // route files
 const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
 
 // dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -19,8 +21,14 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
   );
+});
+
+// handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
