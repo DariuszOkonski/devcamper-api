2e8b0bfc4277e95a10a9f11193b1167493af71f9
Dariusz Okoński <darek200180@gmail.com>
2025-05-22 11:57:02 +0200

    add validation error to error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -4,13 +4,30 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
-  console.log(err.stack.red);
-  console.log(err.name);
+  // console.group('error middleware'.red);
+  // console.log(error);
+  // console.log(err.name.red);
+  // console.groupEnd();
 
   //mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(error.errors)
+      .map((val) => val.message)
+      .join(', ');
+
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
