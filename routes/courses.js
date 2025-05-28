a8b0d3a8051a64b466038478e39c082aa179e3eb
Dariusz Okoński <darek200180@gmail.com>
2025-05-28 15:36:30 +0200

    add endpoint for courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { getCourses } = require('../controllers/courses');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
+// it get request from two endpoints:
+// /api/v1/courses
+// /api/v1/bootcamps/:bootampId/courses
 router.route('/').get(getCourses);
 
 module.exports = router;
