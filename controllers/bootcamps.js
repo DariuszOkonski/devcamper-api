5883ebcc9b02f5714e91216f63559a934f9ea89a
Dariusz Okoński <darek200180@gmail.com>
2025-05-28 14:22:50 +0200

    add filtering to getBootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,7 +5,14 @@ const geocoder = require('../utils/geocoder');
 const { fakeLongLattPositions } = require('../utils/fakeListOfBootcamps');
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+  let queryStr = JSON.stringify(req.query)
+  
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
